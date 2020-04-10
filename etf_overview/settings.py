042cff951525bfa41efc88c1fac7d088776d0405
LeopoldWalther <Leopold.Walther@gmail.com>
2020-04-10 10:27:40 +0200

    feat: Add bootstrap to DjangoWebApp
    - Create base.html with bootstrap styling as HTML templates
    - Change msci.html to extend base.html template
    - Change settings.py to register template directory of base.html

--- etf_overview/settings.py
+++ etf_overview/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'etf_overview.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": ["etf_overview/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
