5d63299479f42613728a49ff56046531fbf75bfe
LeopoldWalther <Leopold.Walther@gmail.com>
2020-06-29 22:01:45 +0200

    feat: Add forms.py

--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,10 @@ def blog_category(request, category):
     posts = Post.objects.filter(categories__name__contains = category).order_by('-created_on')
     context = {"category": category, "posts": posts}
     return render(request, "blog_category.html", context)
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {"post": post, "comments": comments,}
+    return render(request, "blog_detail.html", context)
